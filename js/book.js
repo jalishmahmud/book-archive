5823b98761aa2b9d04a823423ac7c4563ec0d1bf
jalishmahmud <jalish93@gmail.com>
2022-02-17 19:36:17 +0600

    Rest API implement done

--- js/book.js
+++ js/book.js
@@ -1,60 +1,61 @@
 // load books
 const loadBooks = () => {
-    const bookName = document.getElementById('input-book-name').value;
-    document.getElementById('input-book-name').value = '';
-    const url = `https://openlibrary.org/search.json?q=${bookName}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showBooks(data));
-    spinner('block');
+  const bookName = document.getElementById("input-book-name").value;
+  document.getElementById("input-book-name").value = "";
+  const url = `https://openlibrary.org/search.json?q=${bookName}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showBooks(data));
+  spinner("block");
 };
 // spinner
-const spinner = displayValue => {
-    document.getElementById('spinner').style.display = displayValue;
-    bookFound('none', 0);
-    noBookFound('none');
-}
-
-// no book found
-const noBookFound = displayValue => {
-    document.getElementById('no-book-found').style.display = displayValue;
-    document.getElementById('show-books').textContent = '';
+const spinner = (displayValue) => {
+  document.getElementById("spinner").style.display = displayValue;
+  bookFound("none", 0);
+  noBookFound("none");
 };
 
-// book found 
+// no book found
+const noBookFound = (displayValue) => {
+  document.getElementById("no-book-found").style.display = displayValue;
+  document.getElementById("show-books").textContent = "";
+};
+
+// book found
 const bookFound = (displayValue, totalBooksFound) => {
-    document.getElementById('book-found').style.display = displayValue;
-    document.getElementById('book-found').innerHTML = `<h3>${totalBooksFound} books found</h3>`;
+  document.getElementById("book-found").style.display = displayValue;
+  document.getElementById(
+    "book-found"
+  ).innerHTML = `<h3>${totalBooksFound} books found</h3>`;
 };
 
 // show books result
-const showBooks = books => {
-    if (books.numFound === 0) {
-        // if book not found
-        spinner('none');
-        noBookFound('block');
-        bookFound('none', 0)
-    }
-    else {
-        spinner('none');
-        // total book found
-        const totalBooksFound = books.numFound;
-        bookFound('block', totalBooksFound)
-        noBookFound('none');
+const showBooks = (books) => {
+  if (books.numFound === 0) {
+    // if book not found
+    spinner("none");
+    noBookFound("block");
+    bookFound("none", 0);
+  } else {
+    spinner("none");
+    // total book found
+    const totalBooksFound = books.numFound;
+    bookFound("block", totalBooksFound);
+    noBookFound("none");
 
-        const showBooksContainer = document.getElementById('show-books');
+    const showBooksContainer = document.getElementById("show-books");
 
-        // clear previous result
-        showBooksContainer.textContent = '';
+    // clear previous result
+    showBooksContainer.textContent = "";
 
-        const bookList = books.docs;
-        bookList.forEach(book => {
-            // get book image
-            const bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    const bookList = books.docs;
+    bookList.forEach((book) => {
+      // get book image
+      const bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
 
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
             <div class="card">
                 <img src="${bookImage}" class="card-img-top" alt="image not found">
                 <div class="card-body">
@@ -66,22 +67,34 @@ const showBooks = books => {
                             </tr>
                             <tr>
                                 <th scope="row">Author : </th>
-                                <td>${book.author_name ? book.author_name[0] : 'not found'}</td>
+                                <td>${
+                                  book.author_name
+                                    ? book.author_name[0]
+                                    : "not found"
+                                }</td>
                             </tr>
                             <tr>
                                 <th scope="row">Publisher : </th>
-                                <td>${book.publisher ? book.publisher[0] : 'not found'}</td>
+                                <td>${
+                                  book.publisher
+                                    ? book.publisher[0]
+                                    : "not found"
+                                }</td>
                             </tr>
                             <tr>
                                 <th scope="row">Published Year : </th>
-                                <td>${book.first_publish_year ? book.first_publish_year : 'not found'}</td>
+                                <td>${
+                                  book.first_publish_year
+                                    ? book.first_publish_year
+                                    : "not found"
+                                }</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
         `;
-            showBooksContainer.appendChild(div);
-        });
-    };
+      showBooksContainer.appendChild(div);
+    });
+  }
 };
